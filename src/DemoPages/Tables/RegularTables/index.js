57ebb4edef590e00c44e1010818e3534db36e81c
Dansky <lykimphong2001@example.com>
2022-07-14 21:01:49 +0700

    .

--- src/DemoPages/Tables/RegularTables/index.js
+++ src/DemoPages/Tables/RegularTables/index.js
@@ -12,29 +12,66 @@ import PageTitle from '../../../Layout/AppMain/PageTitle';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import TableHover from './Examples/TableHover';
 import {
-    Line,
     ResponsiveContainer,
-    LineChart
 } from 'recharts';
+import {Pie} from 'react-chartjs-2';
 
 import {
     faAngleUp
 } from '@fortawesome/free-solid-svg-icons';
-const data = [
-    {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-    {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-    {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-    {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-    {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-    {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-    {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-    {name: 'Page C', uv: 2000, pv: 6800, amt: 2290},
-    {name: 'Page D', uv: 4780, pv: 7908, amt: 2000},
-    {name: 'Page E', uv: 2890, pv: 9800, amt: 2181},
-    {name: 'Page F', uv: 1390, pv: 3800, amt: 1500},
-    {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-];
-const RegularTables = (props) => {
+import {Doughnut} from 'react-chartjs-2';
+
+function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const getState = () => ({
+    labels: [
+        'Cars',
+        'Trains',
+        'Airplanes'
+    ],
+    datasets: [{
+        data: [getRandomInt(50, 200), getRandomInt(100, 150), getRandomInt(150, 250)],
+        backgroundColor: [
+            '#cbd5dc',
+            '#71deb9',
+            '#ef869e'
+        ],
+        hoverBackgroundColor: [
+            '#7097e1',
+            '#4dd6a7',
+            '#eb6886'
+        ]
+    }]
+});
+export default class RegularTables extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+          data: {
+            labels: ["Đã thanh toán", "Học phí nợ"],
+            datasets: [
+              {
+                data: [
+                  200000000,
+                  100000000,
+                ],
+                backgroundColor: ['#070597',"#9a0505"],
+                hoverBackgroundColor: ['#070597ad', "#9a050591"],
+              },
+            ],
+          },
+          option:{
+            plugins: {
+                legend: {
+                    display: true,
+                }
+            }
+          }
+        };
+    };
+    render() {
     return (
         <Fragment>
             <TransitionGroup>
@@ -64,7 +101,7 @@ const RegularTables = (props) => {
                                         </CardBody>
                                     </Card>
                                     <Row style={{marginLeft:'auto'}}>
-                                    <Card className="main-card mb-3" style={{width:'46%', marginRight:'36px', paddingLeft:'0', height:'250px'}}>
+                                    <Card className="main-card mb-3" style={{width:'46%', marginRight:'100px', paddingLeft:'0', height:'250px'}}>
                                         <CardBody >
                                             <CardTitle style={{textTransform:'none', color:'red', fontSize:'20px'}}>Ghi chú</CardTitle>
                                                 <p style={{color:'black'}}> - Phần mềm tính học phí và hiển thị học phí của sinh viên (SV) đang trong thời gian chạy thử nghiệm. Phòng Đào tạo mong Quý Phụ huynh và SV thông cảm về những sai sót có thể phát sinh trong giai đoạn này.</p>
@@ -72,9 +109,9 @@ const RegularTables = (props) => {
                                                 <p style={{color:'black'}}> - SV có trách nhiệm tự kiểm tra học phí, khi có thắc mắc liên hệ số ĐT: 0353 4138 06 hoặc email: trangnguyen@vido.edu.vn</p>
                                         </CardBody>
                                     </Card>
-                                    <Card className="main-card " style={{width:'50%', display:'flex', height:'auto'}}>
+                                    <Card className="main-card " style={{width:'40%', display:'flex', height:'auto'}}>
                                         <Row>
-                                    <Col lg='4'>
+                                    <Col>
                                     <div className="card mb-3 widget-chart" style={{padding:'1rem 0 0 0',boxShadow:'none'}}>
                                                 <div className="widget-chart-content">
                                                     <div className="icon-wrapper rounded-circle">
@@ -93,71 +130,7 @@ const RegularTables = (props) => {
                                                     </div>
                                                 </div>
                                                 <div className="widget-chart-wrapper chart-wrapper-relative">
-                                                    <ResponsiveContainer height={100}>
-                                                        <LineChart data={data}
-                                                                margin={{top: 5, right: 5, left: 5, bottom: 0}}>
-                                                            <Line type='monotone' dataKey='pv' stroke='#3ac47d'
-                                                                strokeWidth={3}/>
-                                                        </LineChart>
-                                                    </ResponsiveContainer>
-                                                </div>
-                                            </div>
-                                            </Col>
-                                            <Col lg='4'>
-                                    <div className="card mb-3 widget-chart" style={{padding:'1rem 0 0 0',boxShadow:'none'}}>
-                                                <div className="widget-chart-content">
-                                                    <div className="icon-wrapper rounded-circle">
-                                                        <div className="icon-wrapper-bg bg-primary"/>
-                                                        <i className="lnr-cog text-primary"/>
-                                                    </div>
-                                                    <div className="widget-numbers">
-                                                        45.8k
-                                                    </div>
-                                                    <div className="widget-subheading">
-                                                        Đã thanh toán
-                                                    </div>
-                                                    <div className="widget-description text-success">
-                                                        <FontAwesomeIcon icon={faAngleUp}/>
-                                                        <span className="ps-1">175.5%</span>
-                                                    </div>
-                                                </div>
-                                                <div className="widget-chart-wrapper chart-wrapper-relative">
-                                                    <ResponsiveContainer height={100}>
-                                                        <LineChart data={data}
-                                                                margin={{top: 5, right: 5, left: 5, bottom: 0}}>
-                                                            <Line type='monotone' dataKey='pv' stroke='#3ac47d'
-                                                                strokeWidth={3}/>
-                                                        </LineChart>
-                                                    </ResponsiveContainer>
-                                                </div>
-                                            </div>
-                                            </Col>
-                                            <Col lg='4' >
-                                    <div className="card mb-3 widget-chart" style={{padding:'1rem 0 0 0',boxShadow:'none'}}>
-                                                <div className="widget-chart-content" >
-                                                    <div className="icon-wrapper rounded-circle">
-                                                        <div className="icon-wrapper-bg bg-primary"/>
-                                                        <i className="lnr-cog text-primary"/>
-                                                    </div>
-                                                    <div className="widget-numbers">
-                                                        45.8k
-                                                    </div>
-                                                    <div className="widget-subheading">
-                                                        Học phí nợ
-                                                    </div>
-                                                    <div className="widget-description text-success">
-                                                        <FontAwesomeIcon icon={faAngleUp}/>
-                                                        <span className="ps-1">175.5%</span>
-                                                    </div>
-                                                </div>
-                                                <div className="widget-chart-wrapper chart-wrapper-relative">
-                                                    <ResponsiveContainer height={100}>
-                                                        <LineChart data={data}
-                                                                margin={{top: 5, right: 5, left: 5, bottom: 0}}>
-                                                            <Line type='monotone' dataKey='pv' stroke='#3ac47d'
-                                                                strokeWidth={3}/>
-                                                        </LineChart>
-                                                    </ResponsiveContainer>
+                                                        <Doughnut data={this.state.data} width={600} height={250} options={{ maintainAspectRatio: false }}/>
                                                 </div>
                                             </div>
                                             </Col>
@@ -173,5 +146,4 @@ const RegularTables = (props) => {
         </Fragment>
     );
 };
-
-export default RegularTables;
+}
