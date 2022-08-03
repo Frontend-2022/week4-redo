ad4de652d0353e5f94236a9c213bc5ce3b4de2f8
Tạ Đức Minh <107977198+tdminh1@users.noreply.github.com>
2022-08-03 14:50:55 +0700

    Update RegisterContent.js

--- src/DemoPages/Widgets/Register/Examples/RegisterContent.js
+++ src/DemoPages/Widgets/Register/Examples/RegisterContent.js
@@ -2,137 +2,47 @@ import React from "react";
 import "./Register.css";
 import "./RegisterContent.css";
 import { CardTitle, Button } from "reactstrap";
-import { Link } from "react-router-dom";
-import Result from "./Result";
-const RegisterContent = () => {
+
+const RegisterContent = ({ items }) => {
+  // console.log(
+  //   "itemss >> ",
+  //   items.map((item) => item.mhten)
+  // );
   return (
     <>
       <CardTitle>Môn học chờ đăng kí</CardTitle>
       <div className="container">
-        <div className="row sm-gutter container-flow2-1">
-          <div className="col l-3">
-            <div className="container__list-title">
-              <span className="container__title-name">Trong kế hoạch</span>
-              <span className="container__title-number">6 môn</span>
-            </div>
-          </div>
-          <div className="col l-3">
-            <div className="container__list-title">
-              <span className="container__title-name">Học vượt</span>
-              <span className="container__title-number">1 môn</span>
-            </div>
-          </div>
-          <div className="col l-3">
-            <div className="container__list-title">
-              <span className="container__title-name">Học lại</span>
-              <span className="container__title-number">1 môn</span>
-            </div>
+        <div className=" sm-gutter container-flow2-1">
+          <div className="container__list-title">
+            <Button color="success" className="  container__title-name">
+              Trong kế hoạch
+            </Button>
+            <Button color="warning" className=" container__title-name">
+              Học lại
+            </Button>
+            <Button color="warning" className="  container__title-name">
+              Học vượt
+            </Button>
           </div>
         </div>
 
         <div className="row sm-gutter container-flow2-2">
           <div className="col l-3">
             <div className="container__list">
-              <div className="contentsflow2">
-                <div className="Cart_Code">
-                  <div className="Cart">
-                    {" "}
-                    <i className="pe-7s-note2 icon-subject"></i>
-                  </div>
-                  <div className="Code">Nguyên lý lập trình</div>
-                </div>{" "}
-                {/* Cart_Code */}
-                <div className="Content">Mã môn học: </div>
-                <div className="address">Số Tín chỉ: </div>
-                <div className="Order_Info">
-                  <div className="Customer_Infor">
-                    <div className="Name">Giảng Viên: </div>
-                  </div>
-                  <div className="Datetime">Tình trạng:</div>
-
-                  <div className="price-toggle">
-                    <div className="toggle">
-                      <button className="btn-toggle">
-                        <i className="pe-7s-angle-up-circle icon-toggle"></i>
-                        <span>Đăng ký</span>
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="contentsflow2">
-                <div className="Cart_Code">
-                  <div className="Cart">
-                    {" "}
-                    <i className="pe-7s-note2 icon-subject"></i>
-                  </div>
-                  <div className="Code">Nguyên lý lập trình</div>
-                </div>{" "}
-                {/* Cart_Code */}
-                <div className="Content">Mã môn học: </div>
-                <div className="address">Số Tín chỉ: </div>
-                <div className="Order_Info">
-                  <div className="Customer_Infor">
-                    <div className="Name">Giảng Viên: </div>
-                  </div>
-                  <div className="Datetime">Tình trạng:</div>
-
-                  <div className="price-toggle">
-                    <div className="toggle">
-                      <button className="btn-toggle">
-                        <i className="pe-7s-angle-up-circle icon-toggle"></i>
-                        <span>Đăng ký</span>
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="contentsflow2">
-                <div className="Cart_Code">
-                  <div className="Cart">
-                    {" "}
-                    <i className="pe-7s-note2 icon-subject"></i>
-                  </div>
-                  <div className="Code">Nguyên lý lập trình</div>
-                </div>{" "}
-                {/* Cart_Code */}
-                <div className="Content">Mã môn học: </div>
-                <div className="address">Số Tín chỉ: </div>
-                <div className="Order_Info">
-                  <div className="Customer_Infor">
-                    <div className="Name">Giảng Viên: </div>
-                  </div>
-                  <div className="Datetime">Tình trạng:</div>
-
-                  <div className="price-toggle">
-                    <div className="toggle">
-                      <button className="btn-toggle">
-                        <i className="pe-7s-angle-up-circle icon-toggle"></i>
-                        <span>Đăng ký</span>
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col l-3">
-            <div className="container__list">
-              <div className="container__list">
+              {items.map((item) => (
                 <div className="contentsflow2">
                   <div className="Cart_Code">
                     <div className="Cart">
-                      {" "}
                       <i className="pe-7s-note2 icon-subject"></i>
                     </div>
-                    <div className="Code">Nguyên lý lập trình</div>
+                    <div className="Code">{item.mhten}</div>
                   </div>{" "}
                   {/* Cart_Code */}
-                  <div className="Content">Mã môn học: </div>
-                  <div className="address">Số Tín chỉ: </div>
+                  <div className="Content">{`Mã môn học: ${item.mhma}`}</div>
+                  <div className="address">{`Số tín chỉ:  ${item.mhsotc}`}</div>
                   <div className="Order_Info">
                     <div className="Customer_Infor">
-                      <div className="Name">Giảng Viên: </div>
+                      <div className="Name">{`Giảng Viên: ${item.gvten}`} </div>
                     </div>
                     <div className="Datetime">Tình trạng:</div>
 
@@ -146,40 +56,7 @@ const RegisterContent = () => {
                     </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
-          <div className="col l-3">
-            <div className="container__list">
-              <div className="container__list">
-                <div className="contentsflow2">
-                  <div className="Cart_Code">
-                    <div className="Cart">
-                      {" "}
-                      <i className="pe-7s-note2 icon-subject"></i>
-                    </div>
-                    <div className="Code">Nguyên lý lập trình</div>
-                  </div>{" "}
-                  {/* Cart_Code */}
-                  <div className="Content">Mã môn học: </div>
-                  <div className="address">Số Tín chỉ: </div>
-                  <div className="Order_Info">
-                    <div className="Customer_Infor">
-                      <div className="Name">Giảng Viên: </div>
-                    </div>
-                    <div className="Datetime">Tình trạng:</div>
-
-                    <div className="price-toggle">
-                      <div className="toggle">
-                        <button className="btn-toggle">
-                          <i className="pe-7s-angle-up-circle icon-toggle"></i>
-                          <span>Đăng ký</span>
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
