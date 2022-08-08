f6da3b9f8b83d7c6f7d899f59b0c88a6833a164c
Tạ Đức Minh <107977198+tdminh1@users.noreply.github.com>
2022-08-08 17:40:22 +0700

    Update Footer.js

--- src/DemoPages/Widgets/Register/Examples/Footer.js
+++ src/DemoPages/Widgets/Register/Examples/Footer.js
@@ -1,39 +1,35 @@
-import React, {Fragment} from 'react';
-import RegisterModal from './RegisterModal';
-const AppFooter = ({ quantity,tc,cart}) => {
+import React, { Fragment } from "react";
+import RegisterModal from "./RegisterModal";
+const AppFooter = ({ quantity, tc, cart }) => {
+  return (
+    <Fragment>
+      <div className="app-footer">
+        <div className="app-footer__inner">
+          <div className="app-footer-left">
+            <ul className="nav">
+              <li className="nav-item" style={{ marginRight: "30px" }}>
+                Kết quả đăng ký
+              </li>
 
-
-        return (
-            <Fragment>
-                <div className="app-footer">
-                    <div className="app-footer__inner">
-                        <div className="app-footer-left">
-                            <ul className="nav">
-                                <li className="nav-item" style={{marginRight:'30px'}}>
-                                        Kết quả đăng ký
-                                </li>
-                           
-                                <li className="nav-item" style={{marginRight:'30px'}}>
-                                        Tổng số tín chỉ: {tc}
-                                </li>
-                                <li className="nav-item">
-                                        Tổng số môn học: {quantity}
-                                </li>
-                                </ul>
-                        </div>
-                        <div className="app-footer-right">
-                            <ul className="nav">
-                                <li className="nav-item">
-                                        <div className="badge bg-success me-1 ms-0">
-                                            <RegisterModal cart ={cart} />
-                                        </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+              <li className="nav-item" style={{ marginRight: "30px" }}>
+                Tổng số tín chỉ: {tc}
+              </li>
+              <li className="nav-item">Tổng số môn học: {quantity}</li>
+            </ul>
+          </div>
+          <div className="app-footer-right">
+            <ul className="nav">
+              <li style={{ marginBottom: "0 !important" }}>
+                <div className="badge me-1 ms-0">
+                  <RegisterModal cart={cart} />
                 </div>
-            </Fragment>
-        )
-}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
 export default AppFooter;
