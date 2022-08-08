22e661739e4fd23dc98f2c2d06b07266f7c8d546
Tạ Đức Minh <107977198+tdminh1@users.noreply.github.com>
2022-08-08 17:54:43 +0700

    Update RegisterCourse.js

--- src/DemoPages/Widgets/Register/Examples/RegisterCourse.js
+++ src/DemoPages/Widgets/Register/Examples/RegisterCourse.js
@@ -5,10 +5,12 @@ import RegisterContent from "./RegisterContent";
 import axios from "axios";
 import ChooseTabs from "./ChooseTabs";
 import Debt from "./Debt";
+import MoreSubject from "./MoreSubjects";
 class RegisterCoures extends React.Component {
   state = {
     Register: [],
     Debt: [],
+    MoreSubject: [],
   };
   async componentDidMount() {
     var response = await axios.get(
@@ -29,12 +31,23 @@ class RegisterCoures extends React.Component {
         },
       }
     );
+    var MoreSubject = await axios.get(
+      "http://ims-api.viendong.edu.vn/api/beta/hocvien/monhocdukien?hockyid=63&str=2CS",
+      {
+        headers: {
+          token:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTU2NjQsImhvY3ZpZW5pZCI6MTc4ODMsImdpYW5ndmllbmlkIjpudWxsLCJ1c2VyaWQiOiIyMDA1MDIwMDI5IiwicGFzcyI6IjEyMzQ1NiIsImhvY1ZpZW5JZCI6MTc4ODMsImhvY1ZpZW4iOnsiaWQiOjE3ODgzLCJtc2h2IjoiMjAwNTAyMDAyOSIsImhvIjoiVGjDoWkgVGjhu4sgS2ltIiwidGVuIjoiTmfDom4iLCJoaW5oYW5oIjpudWxsLCJtYWxvcCI6IjE0S1RDIiwibmFtbmhhcGhvYyI6MjAyMCwia2hvYWhvYyI6MTQsImdoaWNodSI6Ik5oYXAgdHUgZmlsZSBleGNlbCBEU1NWX0sxNC54bHMiLCJuZ2F5bGFwIjoiMjAyMS0wMS0yOVQxNjoyMjowMC4wMDBaIiwibWF0cmFuZ3RoYWkiOm51bGwsIm5nYXlzaW5oIjoiMjAwMi0wMi0xM1QwMDowMDowMC4wMDBaIiwiZW1haWwiOm51bGwsInNkdCI6IjA4OTgyODAzNzQiLCJjbW5kIjoiMzAxNzk2Mjc3IiwiZ2lvaXRpbmgiOm51bGwsIm5vaXNpbmgiOm51bGwsImtob2luZ2FuaGlkIjo1Njh9LCJnaWFuZ1ZpZW4iOm51bGwsImlhdCI6MTY1Nzk1MTYyOX0.MC_ezgd5xPIax_h6c0xEhjrqppvQ88ZlxnNz4Z6MMsk",
+        },
+      }
+    );
     const data = response?.data?.data ?? [];
     const data2 = resDebt?.data?.data ?? [];
+    const data3 = MoreSubject?.data?.data ?? [];
     // console.log("data", data);
     this.setState({
       Register: data,
       Debt: data2,
+      MoreSubject: data3,
     });
 
     console.log(this.state.Debt);
@@ -51,7 +64,7 @@ class RegisterCoures extends React.Component {
             <Debt items={this.state.Debt} />
           </div>
           <div label="Học vượt">
-            Nothing to see here, this tab is <em>extinct</em>!
+            <MoreSubject items={this.state.MoreSubject} />
           </div>
         </ChooseTabs>
       </>
