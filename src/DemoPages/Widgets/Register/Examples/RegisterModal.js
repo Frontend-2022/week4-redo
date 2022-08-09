04936ffc0c10deec116d0755b98e2fcf1ecda45e
Tạ Đức Minh <107977198+tdminh1@users.noreply.github.com>
2022-08-09 11:41:16 +0700

    Update RegisterModal.js

--- src/DemoPages/Widgets/Register/Examples/RegisterModal.js
+++ src/DemoPages/Widgets/Register/Examples/RegisterModal.js
@@ -1,27 +1,37 @@
-import React, { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalFooter } from 'reactstrap';
-import ModalItem from './ModalItem';
-const RegisterModal = ({cart}) => {
-  const [modal,SetModal] = useState(false)
+import React, { useState } from "react";
+import { Button, Modal, ModalHeader, ModalFooter } from "reactstrap";
+import ModalItem from "./ModalItem";
+const RegisterModal = ({ cart }) => {
+  const [modal, SetModal] = useState(false);
 
-  const toggle = () =>{
-    SetModal(!modal)
-  }
-    return (
-        <span className="d-inline-block mb-2 me-2">
-        <Button color="primary" onClick={toggle}>Icon giỏ hàng / Xem chi tiết</Button>
-        <Modal isOpen={modal} toggle={toggle}>
-          <ModalHeader toggle={toggle} charCode="Y">Kết quả</ModalHeader>
-           <ModalItem cart={cart}/>
-          <ModalFooter>
-            <Button color="link" onClick={toggle}>Hủy</Button>
-            <Button color="primary" onClick={toggle}>Xác nhận</Button>{' '}
-          </ModalFooter>
-        </Modal>
-      </span>
-    );
-  
-}
+  const toggle = () => {
+    SetModal(!modal);
+  };
+  return (
+    <span className="d-inline-block mb-2 me-2">
+      <Button color="success" onClick={toggle}>
+        <i
+          className="pe-7s-pen"
+          style={{ fontWeight: "700", fontSize: "20px" }}
+        ></i>{" "}
+        <span style={{ fontSize: "15px" }}>Xem kết quả</span>
+      </Button>
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle} charCode="Y">
+          Kết quả
+        </ModalHeader>
+        <ModalItem cart={cart} />
+        <ModalFooter>
+          <Button color="link" onClick={toggle}>
+            Hủy
+          </Button>
+          <Button color="primary" onClick={toggle}>
+            Xác nhận
+          </Button>{" "}
+        </ModalFooter>
+      </Modal>
+    </span>
+  );
+};
 
 export default RegisterModal;
-
