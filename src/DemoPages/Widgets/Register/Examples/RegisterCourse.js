14bd3b76580bbfefd06bde9a73c9831645c19c79
Dansky <lykimphong2001@example.com>
2022-08-10 09:41:44 +0700

    commit on update api for RegisterCourse, RegisterHeader, on css for ModalItem

--- src/DemoPages/Widgets/Register/Examples/RegisterCourse.js
+++ src/DemoPages/Widgets/Register/Examples/RegisterCourse.js
@@ -1,11 +1,9 @@
 import React from "react";
 import "./Register.css";
 import RegisterHeader from "./RegisterHeader";
-import RegisterContent from "./RegisterContent";
+
 import axios from "axios";
-import ChooseTabs from "./ChooseTabs";
-import Debt from "./Debt";
-import MoreSubject from "./MoreSubjects";
+
 
 class RegisterCoures extends React.Component {
   state = {
@@ -14,7 +12,6 @@ class RegisterCoures extends React.Component {
     MoreSubject: [],
     Kq: [],
   };
-
   async componentDidMount() {
     var response = await axios.get(
       "http://ims-api.viendong.edu.vn/api/beta/hocvien/monhocdukien?hockyid=63",
@@ -43,46 +40,30 @@ class RegisterCoures extends React.Component {
         },
       }
     );
-    let Kq = await axios.get(
-      "http://ims-api.viendong.edu.vn/api/beta/hocvien/ketquadk?hockyid=63",
-      {
-        headers: {
-          token:
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTU2NjQsImhvY3ZpZW5pZCI6MTc4ODMsImdpYW5ndmllbmlkIjpudWxsLCJ1c2VyaWQiOiIyMDA1MDIwMDI5IiwicGFzcyI6IjEyMzQ1NiIsImhvY1ZpZW5JZCI6MTc4ODMsImhvY1ZpZW4iOnsiaWQiOjE3ODgzLCJtc2h2IjoiMjAwNTAyMDAyOSIsImhvIjoiVGjDoWkgVGjhu4sgS2ltIiwidGVuIjoiTmfDom4iLCJoaW5oYW5oIjpudWxsLCJtYWxvcCI6IjE0S1RDIiwibmFtbmhhcGhvYyI6MjAyMCwia2hvYWhvYyI6MTQsImdoaWNodSI6Ik5oYXAgdHUgZmlsZSBleGNlbCBEU1NWX0sxNC54bHMiLCJuZ2F5bGFwIjoiMjAyMS0wMS0yOVQxNjoyMjowMC4wMDBaIiwibWF0cmFuZ3RoYWkiOm51bGwsIm5nYXlzaW5oIjoiMjAwMi0wMi0xM1QwMDowMDowMC4wMDBaIiwiZW1haWwiOm51bGwsInNkdCI6IjA4OTgyODAzNzQiLCJjbW5kIjoiMzAxNzk2Mjc3IiwiZ2lvaXRpbmgiOm51bGwsIm5vaXNpbmgiOm51bGwsImtob2luZ2FuaGlkIjo1Njh9LCJnaWFuZ1ZpZW4iOm51bGwsImlhdCI6MTY1Nzk1MTYyOX0.MC_ezgd5xPIax_h6c0xEhjrqppvQ88ZlxnNz4Z6MMsk",
-        },
+    let Kq = await axios.get('http://ims-api.viendong.edu.vn/api/v1/hocky', {
+      headers: {
+        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTU2NjQsImhvY3ZpZW5pZCI6MTc4ODMsImdpYW5ndmllbmlkIjpudWxsLCJ1c2VyaWQiOiIyMDA1MDIwMDI5IiwicGFzcyI6IjEyMzQ1NiIsImhvY1ZpZW5JZCI6MTc4ODMsImhvY1ZpZW4iOnsiaWQiOjE3ODgzLCJtc2h2IjoiMjAwNTAyMDAyOSIsImhvIjoiVGjDoWkgVGjhu4sgS2ltIiwidGVuIjoiTmfDom4iLCJoaW5oYW5oIjpudWxsLCJtYWxvcCI6IjE0S1RDIiwibmFtbmhhcGhvYyI6MjAyMCwia2hvYWhvYyI6MTQsImdoaWNodSI6Ik5oYXAgdHUgZmlsZSBleGNlbCBEU1NWX0sxNC54bHMiLCJuZ2F5bGFwIjoiMjAyMS0wMS0yOVQxNjoyMjowMC4wMDBaIiwibWF0cmFuZ3RoYWkiOm51bGwsIm5nYXlzaW5oIjoiMjAwMi0wMi0xM1QwMDowMDowMC4wMDBaIiwiZW1haWwiOm51bGwsInNkdCI6IjA4OTgyODAzNzQiLCJjbW5kIjoiMzAxNzk2Mjc3IiwiZ2lvaXRpbmgiOm51bGwsIm5vaXNpbmgiOm51bGwsImtob2luZ2FuaGlkIjo1Njh9LCJnaWFuZ1ZpZW4iOm51bGwsImlhdCI6MTY1NzY4Nzc3MH0.w9lduwIS3ULlyKaPvaQisrI9_TMF2xj918JpGp_Wauo"
       }
-    );
+    })
 
     const data = response?.data?.data ?? [];
     const data2 = resDebt?.data?.data ?? [];
     const data3 = MoreSubject?.data?.data ?? [];
-    const data4 = Kq?.data ?? [];
-
+    const data4 = Kq.data  ? Kq.data :[];
     this.setState({
       Register: data,
       Debt: data2,
       MoreSubject: data3,
       Kq: data4,
     });
-    // console.log(this.state.Kq);
   }
 
   render() {
     return (
       <>
-        <RegisterHeader items={this.state.Kq} />
+        <RegisterHeader items={this.state.Kq} reg={this.state.Register} debt={this.state.Debt} sub={this.state.MoreSubject}/>
 
-        <ChooseTabs>
-          <div label="Trong kế hoạch">
-            <RegisterContent items={this.state.Register} />
-          </div>
-          <div label="Học lại">
-            <Debt items={this.state.Debt} />
-          </div>
-          <div label="Học vượt">
-            <MoreSubject items={this.state.MoreSubject} />
-          </div>
-        </ChooseTabs>
+        
       </>
     );
   }
--- src/DemoPages/Widgets/Register/Examples/RegisterHeader.js
+++ src/DemoPages/Widgets/Register/Examples/RegisterHeader.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
 import "./RegisterHeader.css";
-import { Nav, NavItem, Button, CardTitle } from "reactstrap";
-import { get } from "react-scroll/modules/mixins/scroller";
-
-const RegisterHeader = ({ items }) => {
-  let gethkid = items.map((a) => a.hockyid);
+import { CardTitle } from "reactstrap";
+import RegisterContent from "./RegisterContent";
+import ChooseTabs from "./ChooseTabs";
+import Debt from "./Debt";
+import MoreSubject from "./MoreSubjects";
+const RegisterHeader = ({ items,reg,debt,sub }) => {
+  useEffect(() => setTerms(items), [items]);
+const [terms, setTerms] = useState([]);
+const changeSelect = (e) => {
+    if (!+e.target.value) {
+    setTerms(reg);
+    return;
+    }
+    setTerms(reg.filter((item) => +item.id === +e.target.value));
+};
+console.log(items)
   return (
     <>
       <form className="register-header">
@@ -12,8 +23,12 @@ const RegisterHeader = ({ items }) => {
           <div>
             <div className="choose-term">
               <div style={{ textAlign: "center" }}>Chọn học kì</div>
-              <select className="slec">
-                <option>{`Học kỳ ${gethkid[0]}`}</option>
+              <select className="slec" onChange={changeSelect}>
+              {items.map((item) => (
+                <>
+                <option  value={+item.id}>{item.ten}</option>
+</>
+              ))}
               </select>
             </div>
           </div>
@@ -29,7 +44,18 @@ const RegisterHeader = ({ items }) => {
         <div className="divider" />
       </form>
       <CardTitle>Môn học chờ đăng kí</CardTitle>
-    </>
+    <ChooseTabs>
+    <div label="Trong kế hoạch">
+      <RegisterContent items={reg} />
+    </div>
+    <div label="Học lại">
+      <Debt items={debt} />
+    </div>
+    <div label="Học vượt">
+      <MoreSubject items={sub} />
+    </div>
+  </ChooseTabs>
+  </>
   );
 };
 export default RegisterHeader;
